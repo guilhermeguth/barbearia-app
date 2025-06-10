7ab5fb14e81e37a17589c75b3c8fc614f2f07f42
Guilherme Guth <guilhermeguthz@gmail.com>
2025-06-09 22:23:17 -0300

    feat: adicionar estrutura inicial de rotas na aplicação

---
+++ backend/src/routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { BarberController } from './controllers/BarberController';
+import { ApiError, BadRequestError } from './helpers/api-errors';
+
+const routes = Router();
+const barberController = new BarberController();
+
+routes.get("/", (req, res) => {
+    throw new BadRequestError("Rota não implementada");
+});
+
+// Rotas para o BarberController
+routes.post("/barber/create", barberController.create);
+routes.patch("/barber/update", barberController.update);
+routes.get("/barber/getAll", barberController.getAll);
+
+export default routes;
