982c18fe1441310f1749ff5ba31b1f07851741e7
Guilherme Guth <guilhermeguthz@gmail.com>
2025-06-20 19:30:53 -0300

    chore: chore: substituído Yarn por NPM

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,13 +1,8 @@
 import { Router } from "express";
 import { BarberController } from "./controllers/BarberController";
-import { ApiError, BadRequestError } from "./helpers/api-errors";
 
 const routes = Router();
 const barberController = new BarberController();
-
-routes.get("/", (req, res) => {
-  throw new BadRequestError("Rota não implementada");
-});
 
 // Rotas para o BarberController
 routes.post("/barber/persist", barberController.persist);
