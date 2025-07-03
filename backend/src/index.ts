2e413834b97117c76d8eb96da9283a27cb5fe33e
Guilherme Guth Zahn <68757836+guilhermeguth@users.noreply.github.com>
2025-07-03 13:24:47 -0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -60,6 +60,8 @@ AppDataSource.initialize().then(async () => {
 
   const PORT = process.env.PORT || 3001;
 
+  const PORT = Number(process.env.PORT) || 3001;
+
   app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server rodando na porta ${PORT}`);
   });
